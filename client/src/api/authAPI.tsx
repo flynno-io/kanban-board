6c10ee25f2c14a96c91302076c0fb40c6036a52b
flynno <luke.laz.flynn@gmail.com>
2024-11-21 18:34:40 -0700

    created login function for client

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,29 @@
-import { UserLogin } from "../interfaces/UserLogin";
+import { UserLogin } from "../interfaces/UserLogin"
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+	try {
+		// Send a POST request to '/auth/login' with user login information in JSON format
+		const response = await fetch("/auth/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(userInfo),
+		})
+
+		// Parse the response body as JSON
+		const data = await response.json()
+
+		// Throw error if response status is not OK (200-299)
+		if (!response.ok) {
+			throw new Error("User information not retrieved, check network tab!")
+		}
+
+		return data // Return the data received from the server
+	} catch (err) {
+		console.log("Error from user login: ", err) // Log any errors that occur during fetch
+		return Promise.reject("Could not fetch user info") // Return a rejected promise with an error message
+	}
 }
 
-
-
-export { login };
+export { login }
